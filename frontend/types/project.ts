d531af506130c5da99e71b55f0060e3f4da45128
HaruPrograming <haruprograming@gmail.com>
2026-03-14 15:39:41 +0900

    feat:type追加

--- frontend/types/project.ts
+++ frontend/types/project.ts
@@ -1,7 +1,13 @@
 export type Project = {
-  id: Key | null | undefined;
-  updated_at(updated_at: any): import("react").ReactNode;
-  created_at(created_at: any): import("react").ReactNode;
+  id: number;
+  project_name: string;
+  creation_level: number;
+  status: number;
+  used_technologies: string;
+  created_at: Date;
+  updated_at: Date;
+};
+export type CreateProject = {
   project_name: string;
   creation_level: number;
   status: number;
